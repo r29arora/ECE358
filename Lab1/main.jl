0da71c8ed11ba3963f1698cf7f4a115d0376bb80
drmcds <edwardwang.94@gmail.com>
2015-09-29 19:24:44 -0400

    Refactor on sudo code

--- Lab1/main.jl
+++ Lab1/main.jl
@@ -1,61 +1,48 @@
-
-function consumer(queueSize, ticTocTime)
-	"""
-	Consumes on tic() and toc() 
-	"""
-	totalTime = randomGenerator(ticTocTime)
-
-	println("This is how much time it took to generate all the packets: ", totalTime)
-end
-
-
-
 function calc_arrival_time()
-	u=rand(0,1) //generate random number between 0...1
-	arrival_time= ((-1/lambda)*log(1-u) * 1000000)
-	return arrival_time
+	return (( -1 / AverageProcessingTime )*log(1 - rand(0:1)) * 1000000)
 end
 
-function arrival()
-	if(t>=packet_arrival_time){
+function arrival(t)
+	if t>=packet_arrival_time
 		packet_queue.add(new_packet)
-		t_arrival=t + calc_arrival_time()
-		t_departure= t+ (packet_size/transmission_rate)
+		t_arrival= t + calc_arrival_time()
+		t_departure= t + (packet_size/transmission_rate)
 		//Also need to consider packet loss case when queue is full
-	}
+	end
 end
 
 function departure()
-	if(t>=t_departure){
+	if(t>=t_departure)
 		queue.pop()
-	}
+	end
 end
 
 main(args[]){
-		intialize_variables(args)
-		t_arrival = calc_arrival_time //calculate first packet arrival time
-		for(i=0;i<=num_of_ticks;i++){
-			arrival()
-			departure()
-		}
-		create_report();
+		
+		
 }
 
 # running the code on the problem instance passed in via command line args.
 if isempty(ARGS)
-	println("M D 1 K ticTocTime")
+	println("M D 1 K AverageProcessingTime")
 else
 	inputIsDeterministic = ARGS[1]
 	outputIsDeterministic = ARGS[2]
 	numOfServers = ARGS[3]
 	queueSize = ARGS[4]
+	AverageProcessingTime = parseint(ARGS[5])
 
-	ticTocTime = parseint(ARGS[5])
+	t_arrival = calc_arrival_time(ticTocTime)
+
+	for i = 0:100
+		arrival()
+		departure()
+	end
 
 
 	println("inputIsDeterministic: ", inputIsDeterministic)
 	println("outputIsDeterministic: ", outputIsDeterministic)
 	println("numOfServers: ", numOfServers)
-	println(consumer(queueSize, ticTocTime))
+	create_report()
 	# println(gtsp_solver(queueSize, true))
 end
