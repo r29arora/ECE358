05cf37e7a8d0a441de9c66d35bf4778f25468d99
drmcds <edwardwang.94@gmail.com>
2015-09-29 20:14:38 -0400

    Finally in a non broken state

--- Lab1/main.jl
+++ Lab1/main.jl
@@ -1,21 +1,26 @@
+using DataStructures
+
 function calc_arrival_time()
 	return (( -1 / AverageProcessingTime )*log(1 - rand(0:1)) * 1000000)
 end
 
-function arrival()
-	if t>=packet_arrival_time
-		packet_queue.add(new_packet)
+function arrival(t_arrival, t_departure, queue, packet_size, transmission_rate)
+	if t >= t_arrival
+		new_packet = 1
+		enqueue!(queue, new_packet)
 		t_arrival= t + calc_arrival_time()
 		t_departure= t + (packet_size/transmission_rate)
-		''' Also need to consider packet loss case when queue is full '''
+		# Also need to consider packet loss case when queue is full '''
 	end
 end
 
-function departure()
-	if(t>=t_departure)
-		queue.pop()
+function departure(t_departure, queue)
+	if t >= t_departure
+		x = dequeue!(queue)
 	end
 end
+
+# ''' t is incrementing up at a constant rate, t_departure is incrementing up at constant rate '''
 
 # running the code on the problem instance passed in via command line args.
 if isempty(ARGS)
@@ -24,20 +29,29 @@ else
 	inputIsDeterministic = ARGS[1]
 	outputIsDeterministic = ARGS[2]
 	numOfServers = ARGS[3]
-	queueSize = ARGS[4]
+	queueSize = parseint(ARGS[4])
 	AverageProcessingTime = parseint(ARGS[5])
+	num_of_ticks = parseint(ARGS[6])
+	t = parseint(ARGS[7])
 
-	t_arrival = calc_arrival_time(ticTocTime)
+	packet_size = 10
+	transmission_rate = 2
 
-	for i = 0:100
-		arrival()
-		departure()
+	queue = Queue(Any)
+
+	t_arrival = calc_arrival_time()
+	t_departure = 10
+
+	for i = 0:num_of_ticks
+		t = i * t
+		arrival(t_arrival, t_departure, queue, packet_size, transmission_rate)
+		departure(t_departure, queue)
 	end
 
 
 	println("inputIsDeterministic: ", inputIsDeterministic)
 	println("outputIsDeterministic: ", outputIsDeterministic)
 	println("numOfServers: ", numOfServers)
-	create_report()
+	# create_report()
 	# println(gtsp_solver(queueSize, true))
 end
