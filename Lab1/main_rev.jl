fda2ec5f01eda890e986a0205cd814f037f689fa
Rajul Arora <r9arora@uwaterloo.ca>
2015-09-30 18:11:03 -0400

    Fixed data received timing

--- Lab1/main_rev.jl
+++ Lab1/main_rev.jl
@@ -10,7 +10,7 @@ end
 avg_process_time = parseint(ARGS[4])
 t = 0
 transmission_rate = 1
-packet_size = 100000
+packet_size = 10
 
 t_arrival = calc_time(avg_process_time)
 t_departure = calc_time(avg_process_time)
@@ -28,15 +28,12 @@ function arrival()
     if t >= t_arrival 
         enqueue!(q, 1)
         t_arrival = t + calc_time(avg_process_time)
-        println("q = ", q)
+#        println("q = ", q)
     end    
     
     if t >= t_departure && !isempty(q)
-#        println("Packet Received ", length(q))
+        println("Packet Received ", length(q))
         dequeue!(q)
-    end
-    
-    if t >= t_arrival
         t_departure = t + (packet_size / transmission_rate)
     end
 end
