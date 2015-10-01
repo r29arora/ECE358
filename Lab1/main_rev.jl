75fb51edb30505ef96995e72e06200faa4c5f9b6
Rajul Arora <r9arora@uwaterloo.ca>
2015-10-01 11:15:37 -0400

    Fixed sized queue

--- Lab1/main_rev.jl
+++ Lab1/main_rev.jl
@@ -1,11 +1,14 @@
 using DataStructures
 
+queue_size = parseint(ARGS[6])
+
 q = Queue(Any)
 dep_q = Queue(Any)
 
 received_packets = 0
 total_packets = 0
 
+idle_time = 0
 total_packet_length = 0
 
 total_q = 0
@@ -22,7 +25,7 @@ packet_size = 50
 
 t_arrival = calc_time(avg_process_time)
 t_departure = calc_time(avg_process_time)
-
+initial_t = calc_time(avg_process_time)
 
 function arrival()
     global t
@@ -36,14 +39,18 @@ function arrival()
     global total_packet_length
             
     if t >= t_arrival 
-        enqueue!(q, 1)
-        t_arrival = t + calc_time(avg_process_time)
-        enqueue!(dep_q, t + (packet_size / transmission_rate))
+        if queue_size != -1 && length(q) >= queue_size 
+            # do nothing, queue is full
+        else 
+            enqueue!(q, 1)
+            t_arrival = t + calc_time(avg_process_time)
+            enqueue!(dep_q, t + (packet_size / transmission_rate)) 
+            total_packet_length = total_packet_length + length(q) 
+        end
+
 #        println("new packet generated", length(q), "in queue")
         total_packets = total_packets + 1
-        total_packet_length = total_packet_length + length(q) 
     end    
-    
 end
 
 function departure()
@@ -51,6 +58,8 @@ function departure()
     global t_departure
     global q
     global received_packets
+    global idle_time
+    global initial_t
     
     if !isempty(dep_q) && !isempty(q)
         earliest_dep = dequeue!(dep_q)
@@ -60,13 +69,13 @@ function departure()
             received_packets = received_packets + 1
         else
             enqueue!(dep_q, earliest_dep)
+            idle_time = idle_time + initial_t
         end
     end
     
 end
 
 num_ticks = parseint(ARGS[5])
-initial_t = calc_time(avg_process_time)
 
 for x = 0:num_ticks
     t = initial_t * x
@@ -79,3 +88,4 @@ println("Total Packets Generated = ", total_packets)
 println("Total Packets Processed = ", received_packets)
 println("Average Number of Packets in the Buffer / Queue = ", total_q / num_ticks)
 println("Sojurn Time = ", total_packet_length * (packet_size / transmission_rate) / total_packets)
+println("Idle Time = ", idle_time)
